27c8c897414bda1a499bac4563a3c1a4aab36ef5
alex <alexizana10@gmail.com>
2021-05-24 12:12:29 +0400

    added styles on Categories component

--- src/components/categoriesBar/CategoriesBar.js
+++ src/components/categoriesBar/CategoriesBar.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './_categoriesBar.scss';
 
+const keywords = [
+  "All",
+  "React Js",
+  "Angular Js",
+  "React Native",
+  "Redux",
+  "Music",
+  "Guitar",
+  "Barcelona",
+  "Zlatan Ibrahimovic",
+  "Fyodor Dostoevsky",
+  "Javascript",
+  "Node Js",
+  "Coding",
+  "Football",
+  "Cricket",
+  "Poor Coder",
+  "Algorithm Art",
+  "Gatsby",
+  "Crime and Punishment",
+  "Graphic Design",
+  "Aldous Huxley",
+  "The Brave New World",
+  "The Brothers Karamazov",
+  "Notes From Underground",
+  "Franz Kafka",
+  "Albert Camus",
+  "The Idiot",
+]
+
 const CategoriesBar = () => {
+
+  const [activeElement, setActiveElement] = useState("All")
+
+  const handleClick = (value) => {
+    setActiveElement(value)
+  }
+
   return(
-    <div>
-      CategoriesBar
+    <div className ="categoriesBar">
+      {
+        keywords.map((value, i) => (
+          <span 
+            key={i} 
+            onClick={() => handleClick(value)}
+            className={activeElement === value ? "active" : ''}
+          >{value}
+          </span>))
+      }
     </div>
   )
 }
